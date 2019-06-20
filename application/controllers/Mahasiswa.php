24420f856e992644e31b3b191cd42e69e92fb297
sakitkepala <andikapriyotama@gmail.com>
2019-06-21 02:14:08 +0700

    Method tambah di controller Mahasiswa

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -18,4 +18,13 @@ class Mahasiswa extends CI_Controller {
     $this->load->view('mahasiswa/index', $data);
     $this->load->view('template/footer');
   }
+
+  public function tambah()
+  {
+    $data['judul_halaman'] = 'Tambah Mahasiswa';
+
+    $this->load->view('template/header', $data);
+    $this->load->view('mahasiswa/tambah');
+    $this->load->view('template/footer');
+  }
 }
