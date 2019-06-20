48f9a8c4784a19c3a3c5fe2541504fa49aa0c367
sakitkepala <andikapriyotama@gmail.com>
2019-06-21 01:17:06 +0700

    Lengkapi template halaman Mahasiswa pakai list bootstrap

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,3 +1,10 @@
 <div class="container">
-  <h3>Daftar Mahasiswa</h3>
+  <div class="row mt-3">
+    <div class="col-md-6">
+      <h3>Daftar Mahasiswa</h3>
+      <ul class="list-group">
+        <li class="list-group-item">Nama Mahasiswa</li>
+      </ul>
+    </div>
+  </div>
 </div>
