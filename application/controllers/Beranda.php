af2796573de64934f29f32efba63c2e0b107a940
sakitkepala <andikapriyotama@gmail.com>
2019-06-20 18:05:41 +0700

    View template header & footer; modif controller beranda untuk load template

--- application/controllers/Beranda.php
+++ application/controllers/Beranda.php
@@ -4,6 +4,10 @@ class Beranda extends CI_Controller {
 
   public function index()
   {
+    $data['judul_halaman'] = 'Beranda Mahasiswa';
+
+    $this->load->view('template/header', $data);
     $this->load->view('beranda/index');
+    $this->load->view('template/footer');
   }
 }
